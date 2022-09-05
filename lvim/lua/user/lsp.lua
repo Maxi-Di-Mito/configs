5aa8e34a8b305f5b796d9269acff1f0a90f64ede
Maximiliano Di Mito <maximiliano.dimito@Maximiliano-Di-Mito.local>
2022-09-05 02:55:18 -0300

    add auto-tag, refactor options

--- lvim/lua/user/lsp.lua
+++ lvim/lua/user/lsp.lua
@@ -10,3 +10,44 @@ end
 if project_has_tailwindcss_dependency() == true then
   require("lvim.lsp.manager").setup "tailwindcss"
 end
+
+-- ESLINT
+local eslint = require 'lspconfig'.eslint
+if eslint then
+  eslint.setup {
+    filetypes = {
+      "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx"
+    }
+  }
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = { "*.tsx", "*.ts", "*.jsx", "*.js" },
+    command = 'EslintFixAll'
+  })
+end
+
+-- SNIPPET CAPABILITY
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- CSS
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require 'lspconfig'.cssls.setup {
+  capabilities = capabilities,
+}
+
+-- JSON
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require 'lspconfig'.jsonls.setup {
+  capabilities = capabilities,
+  settings = {
+    json = {
+      schemas = require('schemastore').json.schemas(),
+      validate = { enable = true },
+    },
+  },
+}
+
+-- HTML
+require 'lspconfig'.html.setup {
+  capabilities = capabilities,
+}
--- lvim/lua/user/options.lua
+++
@@ -1,3 +0,0 @@
--- avoid changing cwd
-lvim.builtin.project.manual_mode = true
-lvim.builtin.nvimtree.setup.update_focused_file.update_cwd = false
