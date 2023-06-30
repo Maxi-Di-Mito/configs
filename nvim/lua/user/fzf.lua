90c93edc97f8d9b782ff4118c9c1af9a43e60920
Maximiliano Di Mito <maximiliano.dimito@Maximiliano-Di-Mito.local>
2023-06-30 16:41:19 -0300

    fix ignore, remove unused mappings

--- nvim/lua/user/fzf.lua
+++ nvim/lua/user/fzf.lua
@@ -2,10 +2,14 @@ local fzf = require("fzf-lua")
 
 fzf.setup({})
 
+-- Viejo para el fzf.vim
 -- Fix to scroll on fzf popup
-vim.cmd([[ autocmd FileType fzf tnoremap <buffer> <C-j> <Down>]])
-vim.cmd([[ autocmd FileType fzf tnoremap <buffer> <C-k> <Up>]])
-vim.cmd([[ autocmd FileType fzf tnoremap <buffer> <C-h> <nop>]])
-vim.cmd([[ autocmd FileType fzf tnoremap <buffer> <C-l> <nop>]])
+--vim.cmd([[ autocmd FileType fzf tnoremap <buffer> <C-j> <Down>]])
+--vim.cmd([[ autocmd FileType fzf tnoremap <buffer> <C-k> <Up>]])
+--vim.cmd([[ autocmd FileType fzf tnoremap <buffer> <C-h> <nop>]])
+--vim.cmd([[ autocmd FileType fzf tnoremap <buffer> <C-l> <nop>]])
 
-vim.env.BAT_THEME = "tokyo-theme-moon"
+vim.cmd([[tnoremap <PageUp> <C-\><C-n>:lua require('fzf-lua.previewer.builtin').base.scroll(-1)<CR>i]])
+vim.cmd([[tnoremap <PageDown> <C-\><C-n>:lua require('fzf-lua.previewer.builtin').base.scroll(1)<CR>i]])
+
+--[[ vim.env.BAT_THEME = "tokyo-theme-moon" ]]
