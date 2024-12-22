86c581ae7d58512e263f128569b945ee70ec47c9
Maximiliano Di Mito <maximiliano.dimito@Maximiliano-Di-Mito.local>
2024-12-22 11:33:50 -0300

    make script work from anywhere, add romfiles to gitignore

--- scripts/roms-downloader/get-rom.bash
+++ scripts/roms-downloader/get-rom.bash
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-console=$(ls -a ./files/ | cat | fzf --tmux 70%)
-selected=$(cat "files/$console" | fzf --tmux 70%)
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+console=$(ls -a "$parent_path/files/" | cat | fzf --tmux 70%)
+selected=$(cat "$parent_path/files/$console" | fzf --tmux 70%)
 
 name=$(echo $selected | cut -f1 -d=)
 url=$(echo $selected | cut -f2 -d=)
 url="${url%' *Size'}"
 size=$(echo $selected | cut -f3 -d=)
 
+echo "$name : SIZE: $size"
+read -p "Download? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 curl -o "$name" "$url"
