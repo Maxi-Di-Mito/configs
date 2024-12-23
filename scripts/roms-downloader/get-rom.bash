9381f4b7e275268a79ed150f433fb7e33a384ba6
Maximiliano Di Mito <maximiliano.dimito@Maximiliano-Di-Mito.local>
2024-12-22 21:10:05 -0300

    make script take optional param to where to look for files

--- scripts/roms-downloader/get-rom.bash
+++ scripts/roms-downloader/get-rom.bash
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+if [ $# -eq 0 ]
+  then
+    parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+else
+parent_path=$1
+fi
+
 console=$(ls -a "$parent_path/files/" | cat | fzf --tmux 70%)
 selected=$(cat "$parent_path/files/$console" | fzf --tmux 70%)
 
@@ -10,4 +16,5 @@ size=$(echo $selected | cut -f3 -d=)
 
 echo "$name : SIZE: $size"
 read -p "Download? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
 curl -o "$name" "$url"
